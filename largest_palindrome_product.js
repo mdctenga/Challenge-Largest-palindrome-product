fd7e2ac502baced39d9d9be2c92d7c4c4d2cb0fd
mdctenga <mdctenga@gmail.com>
2015-06-17 19:32:06 -1000

    version 0.1

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -11,7 +11,34 @@ module.exports = function(multiplicands, digits){
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
-  // do your work here
+
+for (var factor_0 = 1; factor_0 <= 10; factor_0++) {
+	// console.log ("First Number " + factor_0);
+	for (var factor_1 = 1; factor_1 <= 10; factor_1++) {
+		// console.log("Second Number " + factor_1);
+		// multiply both factors
+		var product = factor_0 * factor_1;
+		// console.log(product.toString());
+		console.log(product.split('').reverse().join(''));
+	}
+}
+
+function checkPalindrome(product){
+	console.log(product.toString());
+	// console.log(stringProduct.split('').reverse().join(''));
+}
+// 
+
+ //  var arr = [];
+ //  for (var factor_0 = 999; factor_0>100; factor_0--){
+	// for (var factor_1 = 999; factor_1>100; factor_1--){
+ //  		var mul = factor_1 * factor_0;
+ //  		if (palindromeNumber(mul)){
+ //  			arr.push(factor_1 * factor_0);
+ //  		}
+ //  	}
+ //  }
+
 
 
 
